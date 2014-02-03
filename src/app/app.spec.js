a2fc912f0948ba66c19ca1293584c082d0833564
Johan Hernefeldt <johan.hernefeldt@teliasonera.com>
2014-02-03 13:06:50 +0100

    Translated to english

--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -1,5 +1,5 @@
-describe("Testappen", function() {
-    describe("Reversefiltret", function() {
+describe("ng-spike", function() {
+    describe("The Reverse filter it", function() {
         var reverse;
         beforeEach(module('myApp'));
 
@@ -7,13 +7,13 @@ describe("Testappen", function() {
             reverse = reverseFilter;
         }));
 
-        it("klara tomma strängar", function() {
+        it("handles empty strings", function() {
             expect(reverse('')).toBe('');
         });
-        it("klara strängar av udda längd", function() {
+        it("handles strings of uneven length", function() {
             expect(reverse('Kalle')).toBe('ellaK');
         });
-        it("klara strängar av jämn längd", function() {
+        it("handles strings of even length", function() {
             expect(reverse('Anders')).toBe('srednA');
         });
     });
